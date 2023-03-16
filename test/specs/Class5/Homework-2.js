33eddba85f864d2eacbaca8b86bf8eef30992c29
TonoyGT <rahat.tonoy@ymail.com>
2023-03-16 04:11:43 -0400

    Homework-2

--- test/specs/Class5/Homework-2.js
+++ test/specs/Class5/Homework-2.js
@@ -30,7 +30,7 @@ const { expect } = require("chai");
 const { describe, it } = require("mocha");
 const moment = require("moment");
 
-describe('Homework-1', async () => {
+describe('Homework-2', async () => {
 
     // TC-1: Verify the current date is select by default in Sign Up dropdown
 
@@ -65,15 +65,7 @@ describe('Homework-1', async () => {
 
 
     // TC-2: Verify the travelers count on homepage
-/**
- * 1. Launch hotels.com
- * 2. Make Adults=4 in Room-1
- * 3. Click Add another room
- * 4. Make Adults=3 in Room-2
- * 5. Click on Done button
- * 6. Verify total-travalers is equals to the number of adults mentioned in rooms
- * 
- */
+
 
     it.only('Verify the travelers count on homepage', async () => {
 
@@ -130,16 +122,6 @@ describe('Homework-1', async () => {
        
         let roomTotal= parseFloat(room1AdultsNum) + parseFloat(room2AdultsNum);
         expect(roomTotal, 'Not Equal').to.equal(totalTravelersNum);
-         
-
-
-        /*console.log('Room1=', room1AdultsNum);
-        console.log('Room2=', room2AdultsNum);
-        console.log('TT=', totalTravelersValue);
-        console.log('TT=', totalTravelersNum);
-        console.log('TT=', roomTotal);
-        */
-        
 
     })
 
